196f406230d42fb12374032c21e786d2e66044b7
OlgaShimon <krylov.olga@gmail.com>
2022-01-07 18:24:25 +0200

    adding components folder and navbar

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Get Starting</li>
+        <li>Our services</li>
+        <li>Help</li>
+        <li>About us</li>
+        <li>Partnership</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
